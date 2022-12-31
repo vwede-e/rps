dc7e0e20e140e0bbd361f21aef930bb00bf380d7
vwede-e <115898669+vwede-e@users.noreply.github.com>
2022-12-31 19:35:43 +0100

    added players choice display feature

--- file.js
+++ file.js
@@ -10,17 +10,38 @@ const scissors = document.querySelector(".scissors");
 const paper = document.querySelector(".paper");
 const para = document.querySelector(".para");
 
-rock.addEventListener("click", () => playGame("rock"));
-scissors.addEventListener("click", () => playGame("scissors"));
-paper.addEventListener("click", () => playGame("paper"));
+let div;
 
-//function getUserChoice() {
-   //let choice = "";
-   //while ((choice === null) || (choice.toLowerCase() !== "rock" && 
-      //choice.toLowerCase() !== "paper" && choice.toLowerCase() !== "scissors")) {
-       // choice = prompt("Enter Rock, Paper or Scissors");
-   // }
-    //return choice.toLowerCase();
+rockImage = document.createElement("img");
+rockImage.src = "./images/icon-rock.svg";
+paperImage = document.createElement("img")
+paperImage.setAttribute("src", "./images/icon-paper.svg");
+scissorsImage = document.createElement("img")
+scissorsImage.setAttribute("src", "./images/icon-scissors.svg");
+
+rockImage2 = document.createElement("img");
+rockImage2.src = "./images/icon-rock.svg";
+paperImage2 = document.createElement("img")
+paperImage2.setAttribute("src", "./images/icon-paper.svg");
+scissorsImage2 = document.createElement("img")
+scissorsImage2.setAttribute("src", "./images/icon-scissors.svg");
+
+
+rockImage.style.cssText = paperImage.style.cssText = scissorsImage.style.cssText = "height:200px; width: 200px";
+rockImage2.style.cssText = paperImage2.style.cssText = scissorsImage2.style.cssText = "height:200px; width: 200px"; 
+
+rock.addEventListener("click", () => {
+    computerChoice = getComputerChoice();
+    playGame("rock", computerChoice);
+})
+scissors.addEventListener("click", () => {
+  computerChoice = getComputerChoice();
+  playGame("scissors", computerChoice);
+})
+paper.addEventListener("click", () => {
+    computerChoice = getComputerChoice();
+    playGame("paper", computerChoice);
+})
 
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3)
@@ -38,8 +59,6 @@ function getComputerChoice() {
 }
 
 function playRound(userChoice, computerChoice) {
-    computerChoice = getComputerChoice();
-
     if (userChoice === "paper" && computerChoice ==="rock") {
       return `User Wins This Round: Computer Picked ${computerChoice}`;
           
@@ -54,13 +73,14 @@ function playRound(userChoice, computerChoice) {
         return `A tie. You both picked ${userChoice}`;
     }
     else {
-        return ("Computer Wins This Round");
+        return `Computer picks ${computerChoice} and wins this Round`;
     }
 } 
 
-function playGame(userChoice) {
-    if (userPoint < 10 && computerPoint < 10) {
-        let result = playRound(userChoice, getComputerChoice);
+function playGame(userChoice, computerChoice) {
+    if (userPoint < 5 && computerPoint < 5) {
+        let result = playRound(userChoice, computerChoice);
+        displayUserChoice(userChoice, computerChoice);
         para.textContent = result;
         if (result.includes("User Wins")) {
             userPoint += 1;
@@ -68,7 +88,7 @@ function playGame(userChoice) {
             console.log(`You won a point -- Your score: ${userPoint}; Computer Score: ${computerPoint}`);  
         }
 
-        else if (result.includes("Computer Wins")) {
+        else if (result.includes("Computer picks")) {
             computerPoint +=1;
             computerScoreUI.textContent = computerPoint;
             console.log(`Computer won a point -- Your score: ${userPoint}; Computer Score: ${computerPoint}`);
@@ -98,8 +118,59 @@ function displayFinalResult() {
     para.parentElement.appendChild(button);
     button.addEventListener("click", () => {
         userPoint = computerPoint = 0;
-        userScoreUI.textContent = computerScoreUI.textContent = 0 
+        userScoreUI.textContent = computerScoreUI.textContent = 0;
         para.parentElement.removeChild(button); //removes button element from page
         button = undefined; // set it to undefined
         para.textContent = undefined;}); // set paragraph content to undefined
+        document.body.removeChild(div);
+        div = undefined;
 } 
+
+function displayUserChoice(userChoice, computerChoice) {
+
+    if (div) {
+        document.body.removeChild(div);
+    }
+        //container div
+    div = document.createElement("div");
+    div.setAttribute("class", "icon-container")
+
+    //items div
+    userDiv = document.createElement("div");
+    computerDiv = document.createElement("div"); 
+    userDiv.setAttribute("class", "icon");
+    computerDiv.setAttribute("class", "icon"); 
+    
+    //appending item div to container div
+    div.appendChild(userDiv);
+    div.appendChild(computerDiv)
+
+    userDiv.style.cssText += computerDiv.style.cssText += "height:250px; width:250px";
+    switch (userChoice) {
+        case "rock":
+            userDiv.appendChild(rockImage);
+            break;
+        case "paper":
+            userDiv.appendChild(paperImage);
+            break;
+        case "scissors":
+            userDiv.appendChild(scissorsImage);
+
+    }
+    
+            //computer choice
+    switch (computerChoice) {
+        case "rock":
+            computerDiv.appendChild(rockImage2);
+            break;
+        case "paper":
+            computerDiv.appendChild(paperImage2);
+            break;
+        case "scissors":
+            computerDiv.appendChild(scissorsImage2);
+    }
+
+    document.body.appendChild(div);
+
+    }        
+    
